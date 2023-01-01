18b22102efc636c16f96aa4c078c3cffd972f34c
Richard McHorgh <mchorghr2@gmail.com>
2023-01-01 12:54:45 -0500

    Initial Commit

---
+++ Statusify/StatusifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatusifyApp.swift
+//  Statusify
+//
+//  Created by Richard McHorgh on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct StatusifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
