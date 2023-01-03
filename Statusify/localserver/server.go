e6c2cf9f03384e909dae601a63bef94f48fb0f2b
Richard McHorgh <mchorghr2@gmail.com>
2023-01-03 17:38:34 -0500

    testing opening localhost programatically

--- Statusify/localserver/server.go
+++ Statusify/localserver/server.go
@@ -1,79 +1,68 @@
 package main
 
-import _ "embed"
 import (
+	_ "embed"
 	"fmt"
 	"io"
 	"net/http"
-    "os"
-    "errors"
-    "os/user"
+	"os"
+	"os/exec"
+	"os/user"
 )
 
 // start server
-func mainMux() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler)
-    mux.HandleFunc("/token", handlerRecieve)
-
-	err := http.ListenAndServe(":3000", mux)
-    if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
-	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
-	}
-}
-
 func main() {
-    http.HandleFunc("/", handler)
-    http.HandleFunc("/token", handlerRecieve)
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/token", handlerRecieve)
 
-    http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", nil)
+	go func() {
+		exec.Command("/usr/bin/open", "http://localhost:3000")
+	}()
 }
 
 //go:embed index.html
 var index string
 
-func handler(w http.ResponseWriter, r *http.Request)  {
-    switch r.Method {
-    case http.MethodGet:
-        io.WriteString(w, index)
-    case http.MethodPost:
-        body, err := io.ReadAll(r.Body)
-        if err != nil {
-            http.Error(w, "Couldn't read the request body.'", http.StatusBadRequest)
-            return
-        }
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		io.WriteString(w, index)
+	case http.MethodPost:
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Couldn't read the request body.'", http.StatusBadRequest)
+			return
+		}
 
-        io.WriteString(w, "Recieved.")
-        
-        user, err := user.Current()
-        if err != nil {
-            fmt.Println(err)
-            os.Exit(1)
-            return
-        }
-        
-        f, err := os.Create(user.HomeDir + "/.config/spotifyd/statusify.key")
-        if err != nil {
-            fmt.Println(err)
-            os.Exit(1)
-            return
-        }
-        defer f.Close()
+		io.WriteString(w, "Recieved.")
 
-        f.WriteString(string(body))
-        fmt.Println(string(body))
+		user, err := user.Current()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+			return
+		}
 
-        os.Exit(0)
-        panic("STOP")
-    }
+		f, err := os.Create(user.HomeDir + "/.config/spotifyd/statusify.key")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+			return
+		}
+		defer f.Close()
+
+		f.WriteString(string(body))
+		fmt.Println(string(body))
+
+		os.Exit(0)
+		panic("STOP")
+	}
 }
 
 //go:embed recieve.html
 var recieve string
 
 func handlerRecieve(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, recieve)
+	io.WriteString(w, recieve)
 }
